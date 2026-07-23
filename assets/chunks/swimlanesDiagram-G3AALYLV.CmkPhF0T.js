557ab857563f732dc13e178c3947201b14a9d9d5
ziogn <ziogn@users.noreply.github.com>
2026-07-23 20:10:19 +0800

    deploy: 2026-07-23 20:10:19

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.CmkPhF0T.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.Cci3JPaC.js";import{_ as a}from"../app.BPM6Nc9J.js";import"./chunk-5VM5RSS4.BpRKelb0.js";import"./chunk-XXDRQBXY.Cv4A4QsH.js";import"./chunk-VR4S4FIN.-aY-n8Lt.js";import"./chunk-32BRIVSS.FEtW6SiZ.js";import"./channel.D-H7oxjl.js";import"./framework.CLq1mGhm.js";import"./theme.DbDLYcUV.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
